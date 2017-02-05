2384ff597a67f013b6dafb8e8b4585a855171ad0
Александр Смирнов <Alex@aleksandrs-macbook-air.local>
2017-02-05 10:59:58 +0300

    Initial Commit

---
+++ 1stApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1stApp3
+//
+//  Created by Александр Смирнов on 05/02/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
